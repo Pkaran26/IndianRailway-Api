5a4962720104e21520932878706a6b543c3b2c52
Prateek Kaushik <39780074+Pkaran26@users.noreply.github.com>
2018-06-14 23:17:42 +0530

    Delete trainRoute.py

--- trainRoute.py
+++
@@ -1,8 +0,0 @@
-#!/Users/Prateek/AppData/Local/Programs/Python/Python36-32/python.exe
-import core.core as c
-
-print("Content-Type: application/json")
-print()
-
-ob = c.RailwayApi()
-print(ob.trainRoute("trainnumber"))
