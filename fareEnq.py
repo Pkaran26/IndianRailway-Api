7d855c8815999b5366b7b8a64040803cd352e780
Prateek Kaushik <39780074+Pkaran26@users.noreply.github.com>
2018-06-14 23:17:01 +0530

    Delete fareEnq.py

--- fareEnq.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Prateek/AppData/Local/Programs/Python/Python36-32/python.exe
-import core.core as c
-
-ob = c.RailwayApi()
-print(ob.fareEnq("trainnumber","stncode","destcode","age","classcode","quota","ddate"))
