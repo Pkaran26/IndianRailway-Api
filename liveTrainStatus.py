af9e31bf03cfb176f398da2f21e08a078e2eeeb1
Prateek Kaushik <39780074+Pkaran26@users.noreply.github.com>
2018-06-14 23:16:25 +0530

    Delete liveTrainStatus.py

--- liveTrainStatus.py
+++
@@ -1,8 +0,0 @@
-#!/Users/Prateek/AppData/Local/Programs/Python/Python36-32/python.exe
-import core.core as c
-
-print("Content-Type: application/json")
-print()
-
-ob = c.RailwayApi()
-print(ob.liveTrainStatus("12251","20-06-2018"))
