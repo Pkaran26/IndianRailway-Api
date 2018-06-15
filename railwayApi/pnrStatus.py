e31c372c5d81c332a3563dfe574f2cde8d155363
Prateek Kaushik <39780074+Pkaran26@users.noreply.github.com>
2018-06-16 04:28:30 +0530

    Add files via upload

--- railwayApi/pnrStatus.py
+++ railwayApi/pnrStatus.py
@@ -1,8 +1,14 @@
 #!/Users/Prateek/AppData/Local/Programs/Python/Python36-32/python.exe
-import core.core as c
 
-print("Content-Type: application/json")
+import core.core as c
+import sys, json
+
+print ("Access-Control-Allow-Origin: *")
 print()
 
+myjson = json.load(sys.stdin)
+pnr = myjson['pnr']
+
+
 ob = c.RailwayApi()
-print(ob.pnrStatus("pnr"))
+print(ob.pnrStatus(pnr))
